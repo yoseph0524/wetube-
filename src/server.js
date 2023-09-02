bb03cfbfbfa8616dab267459ae0dabef6f213af7
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-02 14:36:32 -0700

    middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,35 @@
 import express from "express";
 
+const PORT = 4000;
 const app = express();
 
-const handleListening = () => console.log("Server listening on port 4000");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-app.listen(4000, handleListening);
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>NotAllowed</h1>");
+  }
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("what");
+};
+
+const handleProtected = (req, res) => {
+  return res.send("private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
