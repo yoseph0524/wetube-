f112328735a73a5b4db1691042dd615bc83a409c
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-03 22:39:37 -0700

    creating a video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -28,7 +28,19 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
