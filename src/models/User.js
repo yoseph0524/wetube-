4be47e004f039dd2877678d15e154d0928dcf8ce
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-04 18:05:20 -0700

    log out

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,13 @@
-import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
   socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   password: { type: String },
-  name: { type: String },
+  name: { type: String, required: true },
   location: String,
 });
 
