8ee4a2df772ed7c166619c21b4cf8009b0cda132
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-07 16:10:09 -0700

    downloaing with mp4

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,6 @@
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
+import { async } from "regenerator-runtime";
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,10 +8,29 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.2/dist/umd";
+  const ffmpeg = new FFmpeg();
+  ffmpeg.on("log", ({ message }) => console.log(message));
+  const coreResponse = await fetch(`${baseURL}/ffmpeg-core.js`);
+  const wasmResponse = await fetch(`${baseURL}/ffmpeg-core.wasm`);
+  const coreBlob = new Blob([await coreResponse.text()], {
+    type: "text/javascript",
+  });
+  const wasmBlob = new Blob([await wasmResponse.arrayBuffer()], {
+    type: "application/wasm",
+  });
+  const coreURL = URL.createObjectURL(coreBlob);
+  const wasmURL = URL.createObjectURL(wasmBlob);
+  await ffmpeg.load({ coreURL, wasmURL });
+  await ffmpeg.writeFile("recording.webm", await fetchFile(videoFile));
+  await ffmpeg.exec(["-i", "recording.webm", "output.mp4"]);
+  const mp4File = await ffmpeg.readFile("output.mp4");
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const mp4Url = URL.createObjectURL(mp4Blob);
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "MyRecording.webm";
+  a.href = mp4Url;
+  a.download = "My recording.mp4";
   document.body.appendChild(a);
   a.click();
 };
@@ -24,7 +46,7 @@ const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  recorder = new window.MediaRecorder(stream);
+  recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
