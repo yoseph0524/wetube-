ce1c180251c0b9223297957a0ee1dd5be413aaba
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-06 23:24:57 -0700

    Recording the video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,42 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStartBtn = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream;
+let recorder;
+
+const handleDownload = () => {};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new window.MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
 
-startBtn.addEventListener("click", handleStartBtn);
+init();
+
+startBtn.addEventListener("click", handleStart);
