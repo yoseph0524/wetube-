6682bfb4f6d783d5cd5efbdbb4447e24f2647bc6
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-08 09:14:12 -0700

    comment section

---
+++ src/client/js/commentSection.js
@@ -0,0 +1,14 @@
+const videoContainer = document.getElementById("videoContainer");
+const form = document.getElementById("commentForm");
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const textarea = form.querySelector("textarea");
+  const text = textarea.value;
+  const videoId = videoContainer.dataset.id;
+  fetch(`/api/videos/${videoId}/comment`);
+};
+
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
